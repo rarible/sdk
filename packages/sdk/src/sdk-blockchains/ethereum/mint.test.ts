0142cdf620c173203a6457e82f3b17144d8a552f
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-10-28 23:33:53 +0300

    fix: collection to eth collection

--- packages/sdk/src/sdk-blockchains/ethereum/mint.test.ts
+++ packages/sdk/src/sdk-blockchains/ethereum/mint.test.ts
@@ -4,14 +4,14 @@ import Web3 from "web3"
 import { Web3Ethereum } from "@rarible/web3-ethereum"
 import { toBigNumber, toUnionAddress } from "@rarible/types"
 import { MintType } from "../../nft/mint/domain"
-import { createEthereumSdk } from "./index"
+import { createRaribleSdk } from "../../index"
 
 describe("mint", () => {
 	const { provider, wallet } = createE2eProvider()
 	const ethereum = new Web3Ethereum({ web3: new Web3(provider) })
 
 	const ethereumWallet = new EthereumWallet(ethereum, toUnionAddress(wallet.getAddressString()))
-	const sdk = createEthereumSdk(ethereumWallet, "e2e")
+	const sdk = createRaribleSdk(ethereumWallet, "e2e")
 
 	const erc721Address = toUnionAddress("0x22f8CE349A3338B15D7fEfc013FA7739F5ea2ff7")
 	const erc1155Address = toUnionAddress("0x268dF35c389Aa9e1ce0cd83CF8E5752b607dE90d")
@@ -19,14 +19,8 @@ describe("mint", () => {
 	test("should mint ERC721 token", async () => {
 		const sender = await ethereum.getFrom()
 
-		const action = await sdk.nft.mint({
-			collection: {
-				name: "Test-collection",
-				id: erc721Address,
-				features: ["SECONDARY_SALE_FEES", "MINT_AND_TRANSFER"],
-				type: "ERC721",
-			},
-		})
+		const collection = await sdk.apis.collection.getCollectionById({ collection: `ETHEREUM:${erc721Address}` })
+		const action = await sdk.nft.mint({ collection })
 
 		const result = await action.submit({
 			uri: "uri",
@@ -44,14 +38,8 @@ describe("mint", () => {
 	test("should mint ERC1155 token", async () => {
 		const sender = await ethereum.getFrom()
 
-		const action = await sdk.nft.mint({
-			collection: {
-				features: ["MINT_AND_TRANSFER"],
-				id: erc1155Address,
-				name: "Test-collection",
-				type: "ERC1155",
-			},
-		})
+		const collection = await sdk.apis.collection.getCollectionById({ collection: `ETHEREUM:${erc1155Address}` })
+		const action = await sdk.nft.mint({ collection })
 
 		const result = await action.submit({
 			uri: "uri",
