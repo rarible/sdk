9eb9aae52de323e34a4a756b52b7e0e6b01ec313
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-10-28 23:41:12 +0300

    fix: tests

--- packages/sdk/src/sdk-blockchains/ethereum/burn.test.ts
+++ packages/sdk/src/sdk-blockchains/ethereum/burn.test.ts
@@ -1,20 +1,20 @@
 import { Web3Ethereum } from "@rarible/web3-ethereum"
 import { EthereumWallet } from "@rarible/sdk-wallet"
-import { createRaribleSdk } from "@rarible/protocol-ethereum-sdk"
+import { createRaribleSdk as createEthereumSdk } from "@rarible/protocol-ethereum-sdk"
 import { toBigNumber, toUnionAddress } from "@rarible/types"
+import { createRaribleSdk } from "../../index"
 import { initProviders } from "./test/init-providers"
 import { awaitItem } from "./test/await-item"
 import { createTestErc1155, createTestErc721 } from "./test/create-contracts"
-import { createEthereumSdk } from "./index"
 
 describe("burn", () => {
 
 	const { web31, wallet1 } = initProviders({})
 
 	const senderEthereum = new Web3Ethereum({ web3: web31 })
-	const senderSdk = createEthereumSdk(new EthereumWallet(senderEthereum, toUnionAddress(wallet1.getAddressString())), "e2e")
+	const senderSdk = createRaribleSdk(new EthereumWallet(senderEthereum, toUnionAddress(wallet1.getAddressString())), "e2e")
 
-	const raribleSdk = createRaribleSdk(senderEthereum, "e2e")
+	const raribleSdk = createEthereumSdk(senderEthereum, "e2e")
 
 	const contractErc721 = toUnionAddress("0x87ECcc03BaBC550c919Ad61187Ab597E9E7f7C21")
 	const contractErc1155 = toUnionAddress("0x8812cFb55853da0968a02AaaEA84CD93EC4b42A1")
@@ -23,14 +23,8 @@ describe("burn", () => {
 		const contract = createTestErc721(web31, contractErc721)
 		const sender = await senderEthereum.getFrom()
 
-		const mintAction = await senderSdk.nft.mint({
-			collection: {
-				name: "Test-collection",
-				id: contractErc721,
-				features: ["SECONDARY_SALE_FEES"],
-				type: "ERC721",
-			},
-		})
+		const collection = await senderSdk.apis.collection.getCollectionById({ collection: `ETHEREUM:${contractErc721}` })
+		const mintAction = await senderSdk.nft.mint({ collection })
 		const mintResult  = await mintAction.submit({
 			uri: "uri",
 			creators: [{ account: toUnionAddress(sender), value: toBigNumber("10000") }],
@@ -54,14 +48,8 @@ describe("burn", () => {
 		const contract = createTestErc1155(web31, contractErc1155)
 		const sender = await senderEthereum.getFrom()
 
-		const mintAction = await senderSdk.nft.mint({
-			collection: {
-				name: "Test-collection",
-				id: contractErc1155,
-				features: ["SECONDARY_SALE_FEES"],
-				type: "ERC1155",
-			},
-		})
+		const collection = await senderSdk.apis.collection.getCollectionById({ collection: `ETHEREUM:${contractErc1155}` })
+		const mintAction = await senderSdk.nft.mint({ collection })
 		const mintResult  = await mintAction.submit({
 			uri: "uri",
 			creators: [{ account: toUnionAddress(sender), value: toBigNumber("10000") }],
