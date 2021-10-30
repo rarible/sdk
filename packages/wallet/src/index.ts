66fa2d60f0414bf40d1cd3926342305c6e9c5059
Ivan Koryakovtsev <ivan@rarible.com>
2021-10-30 19:10:58 +0300

    feat: improve sdk-wallet, add poly

--- packages/wallet/src/index.ts
+++ packages/wallet/src/index.ts
@@ -1,7 +1,8 @@
 import type { Ethereum } from "@rarible/ethereum-provider"
-import type { Blockchain, UnionAddress } from "@rarible/api-client"
-import { Provider } from "tezos-sdk-module/dist/common/base"
-import { Fcl } from "@rarible/fcl-types"
+import type { UnionAddress } from "@rarible/api-client"
+import type { Provider } from "tezos-sdk-module/dist/common/base"
+import type { Fcl } from "@rarible/fcl-types"
+import type { AbstractWallet, UserSignature } from "./domain"
 
 // @todo replace with types from ethereum-sdk, flow-sdk etc
 
@@ -9,33 +10,17 @@ export type EthereumNetwork = "mainnet" | "ropsten" | "rinkeby" | "e2e"
 export type FlowNetwork = "mainnet" | "testnet"
 export type TezosNetwork = "mainnet" | "granada" | "local"
 
-type SignUserMessageResponse = {
-	signature: string
-}
-
-interface AbstractWallet {
-	blockchain: Blockchain
-	address: UnionAddress
-
-	signPersonalMessage(message: string): Promise<SignUserMessageResponse>
-}
-
 export class EthereumWallet implements AbstractWallet {
 	readonly blockchain = "ETHEREUM"
 
-	constructor(
-		public readonly ethereum: Ethereum,
-		public readonly address: UnionAddress,
-	) {
-	}
+	constructor(public readonly ethereum: Ethereum, public readonly address: UnionAddress) {}
 
-	async signPersonalMessage(message: string): Promise<SignUserMessageResponse> {
-		return { signature: await this.ethereum.personalSign(message) }
+	async signPersonalMessage(message: string): Promise<UserSignature> {
+		return {
+			signature: await this.ethereum.personalSign(message),
+			publicKey: this.address,
+		}
 	}
-}
-
-interface FlowSignedMessageResponse extends SignUserMessageResponse {
-	pubKey: string
 }
 
 export class FlowWallet implements AbstractWallet {
@@ -45,12 +30,11 @@ export class FlowWallet implements AbstractWallet {
 		public readonly fcl: Fcl,
 		public readonly address: UnionAddress,
 		public readonly network: FlowNetwork,
-	) {
-	}
+	) {}
 
-	async signPersonalMessage(message: string): Promise<FlowSignedMessageResponse> {
+	async signPersonalMessage(message: string): Promise<UserSignature> {
 		if (!message.length) {
-			throw Error("Message can't be empty")
+			throw new Error("Message can't be empty")
 		}
 		const messageHex = Buffer.from(message).toString("hex")
 		const currentUser = await this.fcl.currentUser()
@@ -58,39 +42,37 @@ export class FlowWallet implements AbstractWallet {
 
 		const signatures = await currentUser.signUserMessage(messageHex)
 		if (typeof signatures === "string") {
-			throw Error(signatures)
+			throw new Error(signatures)
 		}
 
-		const signature = signatures.find(s => s.addr.toLowerCase() === this.address.toLowerCase())
+		const signature = signatures.find(s => {
+			return s.addr.toLowerCase() === this.address.toLowerCase()
+		})
 		if (signature) {
-			const { keyId } = signature
-			const pubKey = account.keys.find(k => k.index === keyId)
+			const pubKey = account.keys.find(k => k.index === signature.keyId)
 			if (!pubKey) {
-				throw Error(`Key with index "${keyId}" not found on account with address ${this.address}`)
+				throw Error(`Key with index "${signature.keyId}" not found on account with address ${this.address}`)
 			}
 			return {
 				signature: signature.signature,
-				pubKey: pubKey.publicKey,
+				publicKey: pubKey.publicKey,
 			}
-		} else {
-			throw Error(`Signature of user address "${this.address}" not found`)
 		}
+		throw new Error(`Signature of user address "${this.address}" not found`)
 	}
 }
 
 export class TezosWallet implements AbstractWallet {
 	readonly blockchain = "TEZOS"
 
-	constructor(
-		public readonly provider: Provider,
-		public readonly address: UnionAddress,
-	) {
-	}
+	constructor(public readonly provider: Provider, public readonly address: UnionAddress) {}
 
-	async signPersonalMessage(message: string): Promise<SignUserMessageResponse> {
-		// return this.ethereum.personalSign(message)
+	async signPersonalMessage(message: string): Promise<UserSignature> {
 		// @todo implement
-		return { signature: message }
+		return {
+			signature: message,
+			publicKey: this.address,
+		}
 	}
 }
 
