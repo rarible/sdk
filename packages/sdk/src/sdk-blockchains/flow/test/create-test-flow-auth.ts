e428ffa7332f0363bc2c56a49ccdb61be8a820b2
Ivan Koryakovtsev <ivan@rarible.com>
2021-11-04 20:15:08 +0300

    feat: update rarible's dependencies

--- packages/sdk/src/sdk-blockchains/flow/test/create-test-flow-auth.ts
+++ packages/sdk/src/sdk-blockchains/flow/test/create-test-flow-auth.ts
@@ -1,9 +1,9 @@
-import { createTestAuth, TEST_ACCOUNT_1, TEST_ACCOUNT_2 } from "@rarible/flow-test-common"
+import { createTestAuth, FLOW_TEST_ACCOUNT_1, FLOW_TEST_ACCOUNT_2 } from "@rarible/flow-test-common"
 import { Fcl } from "@rarible/fcl-types"
 
 export function createTestFlowAuth(fcl: Fcl) {
-	const authUser1 = createTestAuth(fcl, TEST_ACCOUNT_1.address, TEST_ACCOUNT_1.privKey)
-	const authUser2 = createTestAuth(fcl, TEST_ACCOUNT_2.address, TEST_ACCOUNT_2.privKey)
+	const authUser1 = createTestAuth(fcl, FLOW_TEST_ACCOUNT_1.address, FLOW_TEST_ACCOUNT_1.privKey)
+	const authUser2 = createTestAuth(fcl, FLOW_TEST_ACCOUNT_2.address, FLOW_TEST_ACCOUNT_2.privKey)
 
 	return { authUser1, authUser2 }
 }
