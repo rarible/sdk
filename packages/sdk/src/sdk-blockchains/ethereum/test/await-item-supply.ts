c085ec0f6dfb4a28a5c7b9d54611947439ac579f
Ivan Koryakovtsev <ivan@rarible.com>
2021-11-06 21:42:35 +0300

    chore: retry utility func

--- packages/sdk/src/sdk-blockchains/ethereum/test/await-item-supply.ts
+++ packages/sdk/src/sdk-blockchains/ethereum/test/await-item-supply.ts
@@ -1,10 +1,10 @@
 import type { ItemId } from "@rarible/api-client"
 import type { BigNumber } from "@rarible/types"
 import type { IRaribleSdk } from "../../../domain"
-import { retryBackoff } from "../../../common/retry-backoff"
+import { retry } from "../../../common/retry"
 
 export async function awaitItemSupply(sdk: IRaribleSdk, itemId: ItemId, value: BigNumber) {
-	return retryBackoff(5, 2000, async () => {
+	return retry(5, 2000, async () => {
 		const item = await sdk.apis.item.getItemById({ itemId })
 		if (value.toString() !== item.supply.toString()) {
 			throw new Error("Item value is not equal to the current")
--- packages/sdk/src/sdk-blockchains/ethereum/test/await-stock.ts
+++ packages/sdk/src/sdk-blockchains/ethereum/test/await-stock.ts
@@ -1,9 +1,9 @@
 import type { OrderId } from "@rarible/api-client"
 import type { IRaribleSdk } from "../../../domain"
-import { retryBackoff } from "../../../common/retry-backoff"
+import { retry } from "../../../common/retry"
 
 export async function awaitStock(sdk: IRaribleSdk, id: OrderId, value: string | number) {
-	return retryBackoff(5, 2000, async () => {
+	return retry(5, 2000, async () => {
 		const order = await sdk.apis.order.getOrderById({ id })
 		if (value.toString() !== order.makeStock.toString()) {
 			throw new Error("Stock is not equal")
