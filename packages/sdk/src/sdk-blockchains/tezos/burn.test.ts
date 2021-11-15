11cc671b140cee70873419d48e2891f3f4bb2a66
Aleksandr Sozinov <a.sozinov@rarible.com>
2021-11-15 11:46:21 +0300

    feat: tezos burn method

---
+++ packages/sdk/src/sdk-blockchains/tezos/burn.test.ts
@@ -0,0 +1,87 @@
+// eslint-disable-next-line camelcase
+import { in_memory_provider } from "tezos-sdk-module/dist/providers/in_memory/in_memory_provider"
+import BigNumber from "bignumber.js"
+// eslint-disable-next-line camelcase
+import { deploy_fa2 } from "tezos-sdk-module"
+import { EthereumWallet } from "@rarible/sdk-wallet"
+import type { ItemId } from "@rarible/api-client"
+import { Web3Ethereum } from "@rarible/web3-ethereum"
+import { toUnionAddress } from "@rarible/types"
+import { initProviders } from "../ethereum/test/init-providers"
+import { createRaribleSdk } from "../../index"
+import { MintType } from "../../types/nft/mint/domain"
+
+describe("burn test", () => {
+	const { web31 } = initProviders()
+
+	const ethereum = new Web3Ethereum({ web3: web31 })
+	const wallet = new EthereumWallet(ethereum)
+	const sdk = createRaribleSdk(wallet, "e2e")
+
+	const tezos = in_memory_provider(
+		"edskRzKnQB3jFrx8qYRedDguFNnrmePpvmAyBt6zTz1RzDm3vVnqtrqhhuM8SupK2gTYgq2jdMGJUgvMXJiG5Vz7Wd6Ub2hFTR",
+		// "edsk3UUamwmemNBJgDvS8jXCgKsvjL2NoTwYRFpGSRPut4Hmfs6dG8",
+		"https://granada.tz.functori.com"
+	)
+
+	const config = {
+		exchange: "KT1XgQ52NeNdjo3jLpbsPBRfg8YhWoQ5LB7g",
+		fees: new BigNumber(0),
+	}
+
+	const provider = {
+		tezos,
+		api: "https://rarible-api.functori.com/v0.1/",
+		config,
+	}
+
+	const sender = "tz1dGYcxgScHNkVWdpDKAwuP2xc5afnutjL3"
+	const receipent = "tz1VXxRfyFHoPXBVUrWY5tsa1oWevrgChhSg"
+	let fa2Contract: string = "KT1ChRn258Xwy1wnFMYrU9kFQrDxfJnFm68M"
+	const royaltiesContract: string = "KT1KrzCSQs6XMMRsQ7dqCVcYQeGs7d512zzb"
+	let itemId: ItemId
+
+	beforeAll(async () => {
+		const op = await deploy_fa2(
+			provider,
+			sender,
+			royaltiesContract
+		)
+		console.log("op", op)
+		if (op.contract) {
+			fa2Contract = op.contract
+			console.log("fa2Contract", fa2Contract)
+		}
+
+		await op.confirmation()
+
+		const mintResponse = await sdk.nft.mint({
+			collectionId: toUnionAddress(`TEZOS:${fa2Contract}`),
+		})
+
+		const mintResult = await mintResponse.submit({
+			uri: "",
+			supply: 10,
+			lazyMint: false,
+		})
+
+		itemId = mintResult.itemId
+
+		if (mintResult.type === MintType.ON_CHAIN) {
+			await mintResult.transaction.wait()
+		}
+	}, 1500000)
+
+	test("burn test", async () => {
+
+		const transfer = await sdk.nft.burn({ itemId })
+
+		const result = await transfer.submit({ amount: 1 })
+
+		if (result) {
+		  await result.wait()
+		}
+
+	}, 1500000)
+
+})
---
+++ packages/sdk/src/sdk-blockchains/tezos/burn.ts
@@ -0,0 +1,51 @@
+import type { Maybe } from "@rarible/types/build/maybe"
+import type { Provider } from "tezos-sdk-module/dist/common/base"
+import { toBigNumber } from "@rarible/types"
+import { Action } from "@rarible/action"
+import { burn } from "tezos-sdk-module"
+import { BlockchainTezosTransaction } from "@rarible/sdk-transaction"
+import BigNumber from "bignumber.js"
+import type { BurnRequest, PrepareBurnRequest, PrepareBurnResponse } from "../../types/nft/burn/domain"
+import type { ITezosAPI } from "./common"
+import { getTezosItemData } from "./common"
+
+export class TezosBurn {
+	constructor(
+		private provider: Maybe<Provider>,
+		private apis: ITezosAPI,
+	) {}
+
+	private getRequiredProvider(): Provider {
+		if (!this.provider) {
+			throw new Error("Tezos provider is required")
+		}
+		return this.provider
+	}
+
+	async burn(prepare: PrepareBurnRequest): Promise<PrepareBurnResponse> {
+		const { itemId } = getTezosItemData(prepare.itemId)
+		const item = await this.apis.item.getNftItemById({ itemId })
+
+		return {
+			multiple: true,
+			maxAmount: toBigNumber(item.supply),
+			submit: Action.create({
+				id: "burn" as const,
+				run: async (request: BurnRequest) => {
+					const amount = request?.amount !== undefined ? new BigNumber(request.amount.toFixed()) : undefined
+
+					const result = await burn(
+						this.getRequiredProvider(),
+						{
+							contract: item.contract,
+							token_id: new BigNumber(item.tokenId),
+						},
+						amount
+					)
+
+					return new BlockchainTezosTransaction(result)
+				},
+			}),
+		}
+	}
+}
