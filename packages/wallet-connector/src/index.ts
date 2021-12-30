ed03aa3c9502b32501569451439238c51232e07c
Sergei Vychegzhanin <rzcoder@gmail.com>
2021-12-30 15:26:09 +0500

    fix: export ConnectionState's

--- packages/wallet-connector/src/index.ts
+++ packages/wallet-connector/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./provider"
 export * from "./connector"
+export * from "./connection-state"
 export * from "./connectors/connectors"
 export * from "./common/provider-wallet"
 export * from "./connectors/ethereum/domain"
