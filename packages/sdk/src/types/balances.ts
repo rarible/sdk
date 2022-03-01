77f51bdb7763e9ce9a232bcbd1dda1e33afb62f2
Aleksandr Sozinov <a.sozinov@rarible.com>
2022-03-01 23:38:22 +0300

    chore: get balance and convert readme

--- packages/sdk/src/types/balances.ts
+++ packages/sdk/src/types/balances.ts
@@ -5,6 +5,13 @@ import type { IBlockchainTransaction } from "@rarible/sdk-transaction/src"
 import type { Blockchain } from "@rarible/api-client"
 
 export type IGetBalance = (address: UnionAddress, assetType: AssetType) => Promise<BigNumberValue>
+
+/**
+ * Convert funds to wrapped token or unwrap existed tokens (ex. ETH->wETH, wETH->ETH)
+ * @param blockchain Blockchain where performs operation
+ * @param isWrap Is wrap or unwrap operation
+ * @param value amount of funds to convert
+ */
 export type IConvert = (
 	blockchain: Blockchain, isWrap: boolean, value: BigNumberValue
 ) => Promise<IBlockchainTransaction>
