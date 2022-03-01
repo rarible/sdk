c17382540e9d685556862f849892c45ec15ac283
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-03-01 21:26:37 +0500

    feat: phantom connector

--- packages/connector-phantom/src/domain.ts
+++ packages/connector-phantom/src/domain.ts
@@ -1,15 +1,36 @@
 import type { Transaction } from "@solana/web3.js"
 import type { ProviderConnectionResult } from "@rarible/connector"
 
-export interface ISolanaProviderConnectionResult extends ProviderConnectionResult {
-	publicKey: () => Promise<string>
-	signMessage: (message: Uint8Array) => Promise<Uint8Array>
-	signTransaction: (transaction: Transaction) => Promise<Transaction>
-	signAllTransactions: (transaction: Transaction[]) => Promise<Transaction[]>
+export interface SolanaProviderConnectionResult extends PhantomProvider, ProviderConnectionResult {
 }
 
-export interface ISolanaProvider extends ISolanaProviderConnectionResult {
-	isConnected: boolean
+type DisplayEncoding = "utf8" | "hex"
+type PhantomEvent = "disconnect" | "connect" | "accountChanged"
+type PhantomRequestMethod =
+	| "connect"
+	| "disconnect"
+	| "signTransaction"
+	| "signAllTransactions"
+	| "signMessage"
+
+export interface ConnectOpts {
+	onlyIfTrusted: boolean
+}
+
+type PublicKey = any
+
+export interface PhantomProvider {
+	publicKey: PublicKey | null
+	isConnected: boolean | null
+	signTransaction: (transaction: Transaction) => Promise<Transaction>
+	signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>
+	signMessage: (
+		message: Uint8Array | string,
+		display?: DisplayEncoding
+	) => Promise<any>
+	connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>
 	disconnect: () => Promise<void>
-	connect: (...args: any[]) => Promise<string>
+	on: (event: PhantomEvent, handler: (args: any) => void) => void
+	removeListener: (event: PhantomEvent, handler: (args: any) => void) => void
+	request: (method: PhantomRequestMethod, params: any) => Promise<unknown>
 }
