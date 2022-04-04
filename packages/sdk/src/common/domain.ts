0eedce145d471a935cf7830ebc5763b1fc40c5ae
vitalbadjo <cueage@gmail.com>
2022-04-04 12:31:56 +0400

    fix: remove unused types

--- packages/sdk/src/common/domain.ts
+++ packages/sdk/src/common/domain.ts
@@ -1,7 +1,5 @@
 import type * as ApiClient from "@rarible/api-client"
 import type { Action } from "@rarible/action"
-import type { FlowContractAddress } from "@rarible/flow-sdk/build/common/flow-address"
-import type { Address } from "@rarible/types"
 
 // @todo draft. probably will be changed in future
 export type CurrencyType = {
@@ -24,14 +22,3 @@ export type RequestCurrencyAssetType =
 	| ApiClient.FlowAssetTypeFt
 	| ApiClient.TezosXTZAssetType
 	| ApiClient.TezosFTAssetType
-
-export type AbstractItemId<T extends ApiClient.Blockchain, ContractAddress extends string> = {
-	blockchain: T
-	contract: ContractAddress
-	itemId: string
-}
-
-export type FlowItemId = AbstractItemId<ApiClient.Blockchain.FLOW, FlowContractAddress>
-export type EthereumItemId = AbstractItemId<ApiClient.Blockchain.ETHEREUM, Address>
-export type TezosItemId = AbstractItemId<ApiClient.Blockchain.TEZOS, string>
-export type PolygonItemId = AbstractItemId<ApiClient.Blockchain.POLYGON, string>
