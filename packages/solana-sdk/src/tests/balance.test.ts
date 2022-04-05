1e77d6aa0432eebaf979490e89c5958d68ad4ce0
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-04-05 21:21:28 +0400

    fix: solana balance use numbers instead of BN

--- packages/solana-sdk/src/tests/balance.test.ts
+++ packages/solana-sdk/src/tests/balance.test.ts
@@ -5,7 +5,7 @@ describe("solana sdk balance", () => {
 	const sdk = SolanaSdk.create({ connection: { cluster: "devnet" }, debug: true })
 
 	test("Should check account balance", async () => {
-		const balance = await sdk.balances.getBalance(toPublicKey("6J9aYLQfDWc2QJpXz1M2k1R5AnVGGkqCzYmd3JnVqxB3"))
+		const balance = await sdk.balances.getBalance(toPublicKey("2XyukL1KvwDkfNcdBpfXbj6UtPqF7zcUdTDURNjLFAMo"))
 		expect(parseFloat(balance.toString())).toBeGreaterThan(0)
 	})
 
