f25e68a128d85d9ce0d50ab7447360b2b2331cee
Aleksandr Sozinov <a.sozinov@rarible.com>
2022-04-08 15:45:22 +0300

    chore: build priority

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,6 @@ yarn run build-sdk-wallet
 yarn run build-sdk-transaction
 yarn run build-sdk
 yarn run build-connector
-yarn run build-connector-helper
 yarn run build-connector-iframe
 yarn run build-connector-fcl
 yarn run build-connector-beacon
@@ -14,3 +13,4 @@ yarn run build-connector-torus
 yarn run build-connector-walletlink
 yarn run build-connector-walletconnect
 yarn run build-connector-phantom
+yarn run build-connector-helper
