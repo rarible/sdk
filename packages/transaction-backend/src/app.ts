0a05fde19a80bd5d430fba757a7d6ae7e5a0b320
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-04-20 18:54:49 +0400

    feat: transaction-backend blockchain specific addresses

--- packages/transaction-backend/src/app.ts
+++ packages/transaction-backend/src/app.ts
@@ -11,6 +11,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post("/orders/fill-tx", ordersController.postFillAction)
+app.post("/orders/buy-tx", ordersController.postFillAction)
 app.use(function(req, res){
 	return res
 		.status(404)
