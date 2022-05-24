2452cce8b6eab9ac61e4910559e041b0392234fc
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-05-25 01:08:28 +0400

    test: solana bids test (#229)
    test: solana bids test

--- packages/sdk/src/sdk-blockchains/solana/tests/burn.test.ts
+++ packages/sdk/src/sdk-blockchains/solana/tests/burn.test.ts
@@ -1,34 +1,23 @@
-import { toCollectionId, toCurrencyId, toUnionAddress } from "@rarible/types"
+import { toCurrencyId, toUnionAddress } from "@rarible/types"
 import { SolanaWallet } from "@rarible/sdk-wallet"
 import { createRaribleSdk } from "../../../index"
 import { LogsLevel } from "../../../domain"
 import { getWallet } from "../common/test/test-wallets"
-import { MintType } from "../../../types/nft/mint/domain"
 import { retry } from "../../../common/retry"
+import { mintToken } from "../common/test/mint"
 
 describe("Solana burn", () => {
 	const wallet = getWallet()
 	const sdk = createRaribleSdk(new SolanaWallet(wallet), "development", { logs: LogsLevel.DISABLED })
 
 	test("Should burn NFT", async () => {
-		const mint = await sdk.nft.mint({
-			collectionId: toCollectionId("SOLANA:Ev9n3xAfCrxPrUSUN4mLorwfaknjj4QMcyLUnbPymSmJ"),
-		})
-
-		const mintRes = await mint.submit({
-			supply: 0,
-			lazyMint: false,
-			uri: "https://arweave.net/Vt0uj2ql0ck-U5dLWDWJnwQaZPrvqkfxils8agrTiOc",
-		})
-
-		if (mintRes.type === MintType.ON_CHAIN) {
-			await mintRes.transaction.wait()
-		}
+		const item = await mintToken(sdk)
+		const itemId = item.id
 
 		let balance = await retry(10, 4000, async () => {
 			const balance = await sdk.balances.getBalance(
 				toUnionAddress("SOLANA:" + wallet.publicKey),
-				{ "@type": "SOLANA_NFT", itemId: mintRes.itemId },
+				{ "@type": "SOLANA_NFT", itemId },
 			)
 			if (parseFloat(balance.toString()) < 1) {
 				throw new Error(`Wrong balance value. Expected ${1}. Actual: ${parseFloat(balance.toString())}`)
@@ -38,7 +27,7 @@ describe("Solana burn", () => {
 		expect(parseFloat(balance.toString())).toBeGreaterThanOrEqual(1)
 
 		const tx = await retry(10, 4000, async () => {
-			const burn = await sdk.nft.burn({ itemId: mintRes.itemId })
+			const burn = await sdk.nft.burn({ itemId })
 			return burn.submit({ amount: parseFloat(balance.toString()) })
 		})
 		await tx?.wait()
@@ -46,7 +35,7 @@ describe("Solana burn", () => {
 		balance = await retry(10, 4000, async () => {
 			const balance = await sdk.balances.getBalance(
 				toUnionAddress("SOLANA:" + wallet.publicKey),
-				toCurrencyId(mintRes.itemId),
+				toCurrencyId(itemId),
 			)
 			if (parseFloat(balance.toString()) !== 0) {
 				throw new Error(`Wrong balance value. Expected ${0}. Actual: ${parseFloat(balance.toString())}`)
--- packages/sdk/src/sdk-blockchains/solana/tests/sell-update.test.ts
+++ packages/sdk/src/sdk-blockchains/solana/tests/sell-update.test.ts
@@ -1,32 +1,18 @@
 import { SolanaWallet } from "@rarible/sdk-wallet"
-import { toBigNumber, toCollectionId } from "@rarible/types"
+import { toBigNumber } from "@rarible/types"
 import { createRaribleSdk } from "../../../index"
 import { LogsLevel } from "../../../domain"
 import { getWallet } from "../common/test/test-wallets"
-import { MintType } from "../../../types/nft/mint/domain"
 import { retry } from "../../../common/retry"
+import { mintToken } from "../common/test/mint"
 
 describe("Solana sell order update", () => {
 	const wallet = getWallet(0)
 	const sdk = createRaribleSdk(new SolanaWallet(wallet), "development", { logs: LogsLevel.DISABLED })
 
 	test("Should set item to sell & change price", async () => {
-		const mint = await sdk.nft.mint({
-			collectionId: toCollectionId("SOLANA:Ev9n3xAfCrxPrUSUN4mLorwfaknjj4QMcyLUnbPymSmJ"),
-		})
-
-		const mintRes = await mint.submit({
-			supply: 0,
-			lazyMint: false,
-			uri: "https://arweave.net/Vt0uj2ql0ck-U5dLWDWJnwQaZPrvqkfxils8agrTiOc",
-		})
-
-		expect(mintRes.itemId).toBeTruthy()
-		if (mintRes.type === MintType.ON_CHAIN) {
-			await mintRes.transaction.wait()
-		}
-
-		const itemId = mintRes.itemId
+		const item = await mintToken(sdk)
+		const itemId = item.id
 
 		const orderId = await retry(10, 4000, async () => {
 			const sell = await sdk.order.sell({ itemId })
