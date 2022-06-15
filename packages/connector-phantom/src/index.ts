9aeaaadfc4fd0d956c829243de8014ee1f21232a
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-06-15 18:00:23 +0400

    fix: allow connectors work with rxjs 7.x

--- packages/connector-phantom/src/index.ts
+++ packages/connector-phantom/src/index.ts
@@ -131,6 +131,6 @@ export class PhantomConnectionProvider extends
 
 	async isConnected(): Promise<boolean> {
 		const instance = await this.instance.pipe(first()).toPromise()
-		return !!instance.isConnected
+		return !!instance?.isConnected
 	}
 }
