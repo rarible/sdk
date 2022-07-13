35d3768aa0db9bf6b637540e4a5a4a0ed06f7048
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-07-13 16:07:30 +0400

    test: remove .env

--- packages/sdk/src/sdk-blockchains/solana/common/test/create-sdk.ts
+++ packages/sdk/src/sdk-blockchains/solana/common/test/create-sdk.ts
@@ -7,7 +7,7 @@ import { LogsLevel } from "../../../../domain"
 
 export function createSdk(wallet: SolanaWalletProvider): IRaribleSdk {
 	const endpoint = process.env.SOLANA_CUSTOM_ENDPOINT !== "" ? process.env.SOLANA_CUSTOM_ENDPOINT : undefined
-	console.log("solana endpoint:", endpoint)
+	console.debug("solana endpoint:", endpoint)
 
 	return createRaribleSdk(new SolanaWallet(wallet), "development", {
 		logs: LogsLevel.DISABLED,
