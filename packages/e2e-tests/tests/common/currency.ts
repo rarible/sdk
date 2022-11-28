46161a5cd0051b851569ac0cf4af4b414f8714fc
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-11-28 18:06:32 +0500

    test: use fixed erc20 contract in e2e tests (#396)

--- packages/e2e-tests/tests/common/currency.ts
+++ packages/e2e-tests/tests/common/currency.ts
@@ -1,28 +1,36 @@
 import type { RequestCurrency } from "@rarible/sdk/build/common/domain"
 import type { BlockchainWallet } from "@rarible/sdk-wallet"
 import { EthereumWallet, FlowWallet, SolanaWallet, TezosWallet } from "@rarible/sdk-wallet"
-import { toContractAddress } from "@rarible/types"
-import { deployTestErc20 } from "@rarible/ethereum-sdk-test-common"
+import { toContractAddress, toAddress } from "@rarible/types"
+import { getTestErc20Contract } from "@rarible/ethereum-sdk-test-common"
 import { getWalletAddressFull } from "./wallet"
 import { testsConfig } from "./config"
 import { Logger } from "./logger"
 
 
-export async function getCurrency(wallets: { seller: BlockchainWallet, buyer: BlockchainWallet },
-	currency: string): Promise<RequestCurrency> {
+export async function getCurrency(
+	wallets: { seller: BlockchainWallet, buyer: BlockchainWallet },
+	currency: string,
+	mint: boolean = false
+): Promise<RequestCurrency> {
 	Logger.log(`Get currency for=${currency}`)
 	if (wallets.seller instanceof EthereumWallet && wallets.buyer instanceof EthereumWallet) {
 		if (currency === "ERC20") {
-			const testErc20 = await deployTestErc20((wallets.seller.ethereum as any).config.web3, "test erc20", "TST20")
-			const addressBuyer = await getWalletAddressFull(wallets.buyer)
-			const addressSeller = await getWalletAddressFull(wallets.seller)
-			await testErc20.methods.mint(addressBuyer.address, 1000).send({
-				from: addressSeller.address,
-				gas: 200000,
-			})
+			const testErc20 = getTestErc20Contract(
+				(wallets.seller.ethereum as any).config.web3,
+				toAddress(testsConfig.variables.ETHEREUM_ERC20)
+			)
+			if (mint) {
+				const addressBuyer = await getWalletAddressFull(wallets.buyer)
+				const addressSeller = await getWalletAddressFull(wallets.seller)
+				await testErc20.methods.mint(addressBuyer.address, "1000000000000000000000000000").send({
+					from: addressSeller.address,
+					gas: 200000,
+				})
+			}
 			return {
 				"@type": "ERC20",
-				contract: toContractAddress(`ETHEREUM:${testErc20.options.address}`),
+				contract: toContractAddress(`ETHEREUM:${testsConfig.variables.ETHEREUM_ERC20}`),
 			}
 		}
 		if (currency === "ETH") {
