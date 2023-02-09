a644d8af5356a4c3c79cbb4fad5f0c84e1702edb
Aleksandr Sozinov <a.sozinov@rarible.com>
2023-02-09 11:22:44 +0300

    feat: migrate to walletconnect 2.0

--- packages/connector-walletconnect/src/index.ts
+++ packages/connector-walletconnect/src/index.ts
@@ -1,20 +1,21 @@
 import type { Observable } from "rxjs"
 import { first, mergeMap, startWith } from "rxjs/operators"
-import type WalletConnectProvider from "@walletconnect/web3-provider"
-import type { IWalletConnectProviderOptions } from "@walletconnect/types"
 import type { ConnectionState, EthereumProviderConnectionResult, Maybe } from "@rarible/connector"
 import { AbstractConnectionProvider, cache, connectToWeb3, getStateConnecting } from "@rarible/connector"
-
+import type {
+	EthereumProviderOptions,
+	EthereumProvider,
+} from "@walletconnect/ethereum-provider/dist/types/EthereumProvider"
 
 const PROVIDER_ID = "walletconnect" as const
 
 export class WalletConnectConnectionProvider extends
 	AbstractConnectionProvider<typeof PROVIDER_ID, EthereumProviderConnectionResult> {
-	private readonly instance: Observable<WalletConnectProvider>
+	private readonly instance: Observable<EthereumProvider>
 	private readonly connection: Observable<ConnectionState<EthereumProviderConnectionResult>>
 
 	constructor(
-		private readonly config: IWalletConnectProviderOptions
+		private readonly config: EthereumProviderOptions
 	) {
 		super()
 		this.instance = cache(() => this._connect())
@@ -27,9 +28,9 @@ export class WalletConnectConnectionProvider extends
 		)
 	}
 
-	private async _connect(): Promise<WalletConnectProvider> {
-		const { default: WalletConnectProvider } = await import("@walletconnect/web3-provider")
-		const provider = new WalletConnectProvider(this.config)
+	private async _connect(): Promise<EthereumProvider> {
+		const { default: EthereumProvider } = await import("@walletconnect/ethereum-provider")
+		const provider = await EthereumProvider.init(this.config)
 		await provider.enable()
 		return provider
 	}
