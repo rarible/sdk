1efd60ddaf28257028ba82d0246b19e83bb8af37
Aleksandr Sozinov <sanzeting@mail.ru>
2023-03-30 19:04:08 +0300

    fix: e2e tests dev (#452)
    * fix: e2e tests dev
    
    * chore: eth upsert order test
    
    * chore: tests fix
    
    ---------
    
    Co-authored-by: Aleksandr Sozinov <a.sozinov@rarible.com>

--- packages/e2e-tests/tests/ethereum/smoke/order/mint-floorbid-cancel.test.ts
+++ packages/e2e-tests/tests/ethereum/smoke/order/mint-floorbid-cancel.test.ts
@@ -207,14 +207,14 @@ describe.each(suites())("$blockchain mint => floorBid => cancel", (suite) => {
 
 		const bidOrder = await bid(buyerSdk, buyerWallet, { collectionId: collection.id }, bidRequest)
 
-		await retry(10, 2000, async () => {
+		await retry(40, 3000, async () => {
 			const collection1 = await getCollection(sellerSdk, address)
 			expect(collection1.bestBidOrder?.takePrice).toBe(bidRequest.price)
 		})
 
 		await cancel(buyerSdk, buyerWallet, { orderId: bidOrder.id })
 
-		await retry(10, 2000, async () => {
+		await retry(40, 3000, async () => {
 			const collection2 = await getCollection(sellerSdk, address)
 			expect(collection2.bestBidOrder).toBe(undefined)
 		})
--- packages/e2e-tests/tests/ethereum/smoke/order/mint-sell-buy.test.ts
+++ packages/e2e-tests/tests/ethereum/smoke/order/mint-sell-buy.test.ts
@@ -279,10 +279,16 @@ describe.each(suites())("$blockchain mint => sell => buy", (suite) => {
 
 		const collection = await getCollection(sellerSdk, suite.collectionId)
 
-		const { nft } = await mint(sellerSdk, sellerWallet, { collection },
-			suite.mintRequest(walletAddressSeller.unionAddress))
+		const { nft } = await mint(
+			sellerSdk,
+			sellerWallet,
+			{ collection },
+			suite.mintRequest(walletAddressSeller.unionAddress)
+		)
 
+		console.log("after mint")
 		const requestCurrency = await getCurrency(suite.wallets, suite.currency)
+		console.log("after get currency")
 		const orderRequest = await suite.sellRequest(requestCurrency)
 
 		const sellOrder = await sell(sellerSdk, sellerWallet, { itemId: nft.id }, orderRequest)
