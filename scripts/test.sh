bc68c194750ee972d366bad428c9af15dbe0acb5
Aleksandr Sozinov <a.sozinov@rarible.com>
2023-06-05 16:05:15 +0300

    chore: log

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "testnet key="$SDK_API_KEY_TESTNET
+echo "prod key="$SDK_API_KEY_PROD
 lerna run test --parallel --ignore=@rarible/sdk || echo 'ignoring errors in test results'
 lerna run test --scope=@rarible/sdk  || echo 'ignoring errors in SDK test results'
