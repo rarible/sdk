40c122e015b465f5e6a4bcb77ea7a7b0b555e9f2
André Ginja <andginja@gmail.com>
2023-11-08 16:19:25 +0000

    feat(WL-2132): add Firebase Connector to SDK
    Adding the firebase connector to the SDK

--- packages/example/src/components/connector/connectors-setup.ts
+++ packages/example/src/components/connector/connectors-setup.ts
@@ -1,12 +1,20 @@
 import { NetworkType as TezosNetwork } from "@airgap/beacon-sdk"
 import type { RaribleSdkEnvironment } from "@rarible/sdk/build/config/domain"
-import type { ConnectionProvider, IConnectorStateProvider } from "@rarible/connector"
-import { Connector, DappType, InjectedWeb3ConnectionProvider } from "@rarible/connector"
+import type {
+	ConnectionProvider,
+	IConnectorStateProvider,
+} from "@rarible/connector"
+import {
+	Connector,
+	DappType,
+	InjectedWeb3ConnectionProvider,
+} from "@rarible/connector"
 import { FclConnectionProvider } from "@rarible/connector-fcl"
 import { MEWConnectionProvider } from "@rarible/connector-mew"
 import { NFIDConnectionProvider } from "@rarible/connector-nfid"
 import { BeaconConnectionProvider } from "@rarible/connector-beacon"
 import { TorusConnectionProvider } from "@rarible/connector-torus"
+import { FirebaseConnectionProvider } from "@rarible/connector-firebase"
 import { WalletLinkConnectionProvider } from "@rarible/connector-walletlink"
 import { PhantomConnectionProvider } from "@rarible/connector-phantom"
 import { SolflareConnectionProvider } from "@rarible/connector-solflare"
@@ -119,119 +127,159 @@ export function getConnector(environment: RaribleSdkEnvironment) {
 	const flowNetwork = environmentToFlowNetwork(environment)
 	const tezosNetwork = environmentToTezosNetwork(environment)
 
-	const injected = mapEthereumWallet(new InjectedWeb3ConnectionProvider({
-		prefer: [DappType.Metamask],
-	}))
+	const injected = mapEthereumWallet(
+		new InjectedWeb3ConnectionProvider({
+			prefer: [DappType.Metamask],
+		})
+	)
 
-	const mew = mapEthereumWallet(new MEWConnectionProvider({
-		networkId: ethChainId,
-		rpcUrl: ethereumRpcMap[ethChainId],
-	}))
+	const mew = mapEthereumWallet(
+		new MEWConnectionProvider({
+			networkId: ethChainId,
+			rpcUrl: ethereumRpcMap[ethChainId],
+		})
+	)
 
-	const nfid = mapEthereumWallet(new NFIDConnectionProvider({
-		origin: process.env.REACT_APP_NFID_ORIGIN || "https://nfid.one",
-	}))
+	const nfid = mapEthereumWallet(
+		new NFIDConnectionProvider({
+			origin: process.env.REACT_APP_NFID_ORIGIN || "https://nfid.one",
+		})
+	)
 
-	const beacon: ConnectionProvider<"beacon", IWalletAndAddress> = mapTezosWallet(new BeaconConnectionProvider({
-		appName: "Rarible Test",
-		accessNode: tezosNetwork.accessNode,
-		network: tezosNetwork.network,
-	}))
+	const beacon: ConnectionProvider<"beacon", IWalletAndAddress> =
+    mapTezosWallet(
+    	new BeaconConnectionProvider({
+    		appName: "Rarible Test",
+    		accessNode: tezosNetwork.accessNode,
+    		network: tezosNetwork.network,
+    	})
+    )
 
-	const fcl = mapFlowWallet(new FclConnectionProvider({
-		accessNode: flowNetwork.accessNode,
-		walletDiscovery: flowNetwork.walletDiscovery,
-		network: flowNetwork.network,
-		applicationTitle: "Rari Test",
-		applicationIcon: "https://rarible.com/favicon.png?2d8af2455958e7f0c812",
-	}))
+	const fcl = mapFlowWallet(
+		new FclConnectionProvider({
+			accessNode: flowNetwork.accessNode,
+			walletDiscovery: flowNetwork.walletDiscovery,
+			network: flowNetwork.network,
+			applicationTitle: "Rari Test",
+			applicationIcon: "https://rarible.com/favicon.png?2d8af2455958e7f0c812",
+		})
+	)
 
-	const magic = mapFlowWallet(new MattelConnectionProvider({
-		magicProviderId: "JeTIUJ7GJNnfwnxMwtPRa7JzbzRVxA4p3TdQsXryggM=",
-		magicAPIKey: "pk_live_63A5A557D1D4882D",
-		auth0Domain: "login-test.mattel.com",
-		auth0ClientId: "nXpDI0BnWhxB5DIhQVGOrB2LwgOvKIhd",
-		auth0RedirectUrl: "https://test-virtual.mattel.com",
-		accessNode: flowNetwork.accessNode,
-		network: flowNetwork.network,
-	}) as any) as any
+	const magic = mapFlowWallet(
+		new MattelConnectionProvider({
+			magicProviderId: "JeTIUJ7GJNnfwnxMwtPRa7JzbzRVxA4p3TdQsXryggM=",
+			magicAPIKey: "pk_live_63A5A557D1D4882D",
+			auth0Domain: "login-test.mattel.com",
+			auth0ClientId: "nXpDI0BnWhxB5DIhQVGOrB2LwgOvKIhd",
+			auth0RedirectUrl: "https://test-virtual.mattel.com",
+			accessNode: flowNetwork.accessNode,
+			network: flowNetwork.network,
+		}) as any
+	) as any
 
 	let torus = undefined
 	if (isEthNetwork) {
-		torus = mapEthereumWallet(new TorusConnectionProvider({
-			network: {
-				host: ethNetworkName,
-			},
-		}))
+		torus = mapEthereumWallet(
+			new TorusConnectionProvider({
+				network: {
+					host: ethNetworkName,
+				},
+			})
+		)
 	}
 
-	const walletLink = mapEthereumWallet(new WalletLinkConnectionProvider({
-		networkId: ethChainId,
-		estimationUrl: ethereumRpcMap[ethChainId],
-		url: ethereumRpcMap[ethChainId],
-	}, {
-		appName: "Rarible",
-		appLogoUrl: "https://rarible.com/static/logo-500.static.png",
-		darkMode: false,
-	}))
+	let firebase = undefined
+	if (isEthNetwork) {
+		firebase = mapEthereumWallet(
+			new FirebaseConnectionProvider(
+				"BBD0kzmxWBstkgHeJsQqwiF7RbVgmA7ReBRIyw2GRJoCHJTuCAXHD8pwX3PtotSwwh0EMoBZVgVjRss6jKq8Kg8",
+				{
+					chainNamespace: "eip155",
+					chainId: "0x13881",
+					rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
+					displayName: "Polygon Mumbai Testnet",
+					blockExplorer: "https://mumbai.polygonscan.com/",
+					ticker: "MATIC",
+					tickerName: "Matic",
+				},
+				{
+					apiKey: "AIzaSyD7h1O-nf40cRyLpP9F_Wl1Z_zuZYyZh5Y",
+					authDomain: "dogami-auth.firebaseapp.com",
+					projectId: "dogami-auth",
+					storageBucket: "dogami-auth.appspot.com",
+					messagingSenderId: "741349520212",
+					appId: "1:741349520212:web:8acb236f44ddd005adcec1",
+				},
+				"testnet"
+			)
+		)
+	}
 
-	const walletConnectV2 = mapEthereumWallet(new WalletConnectConnectionProviderV2({
-		projectId: "4f9fb88799dfa8d3654bdd130be840f2",
-		chains: [ethChainId],
-		optionalChains: Object.keys(ethereumRpcMap)
-			.map(x => +x)
-			.filter(x => x !== ethChainId),
-		showQrModal: true,
-		methods: [
-			"eth_sendTransaction",
-			"personal_sign",
-			"eth_signTypedData",
-		],
-		optionalMethods: [
-			"eth_accounts",
-			"eth_requestAccounts",
-			"eth_sendRawTransaction",
-			"eth_sign",
-			"eth_signTransaction",
-			"eth_signTypedData",
-			"eth_signTypedData_v3",
-			"eth_signTypedData_v4",
-			"wallet_switchEthereumChain",
-			"wallet_addEthereumChain",
-			"wallet_getPermissions",
-			"wallet_requestPermissions",
-			"wallet_registerOnboarding",
-			"wallet_watchAsset",
-			"wallet_scanQRCode",
-		],
-		events: [
-			"chainChanged",
-			"accountsChanged",
-		],
-		optionalEvents: [
-			"message",
-			"disconnect",
-			"connect",
-		],
-		rpcMap: ethereumRpcMap,
-	}))
+	const walletLink = mapEthereumWallet(
+		new WalletLinkConnectionProvider(
+			{
+				networkId: ethChainId,
+				estimationUrl: ethereumRpcMap[ethChainId],
+				url: ethereumRpcMap[ethChainId],
+			},
+			{
+				appName: "Rarible",
+				appLogoUrl: "https://rarible.com/static/logo-500.static.png",
+				darkMode: false,
+			}
+		)
+	)
+
+	const walletConnectV2 = mapEthereumWallet(
+		new WalletConnectConnectionProviderV2({
+			projectId: "4f9fb88799dfa8d3654bdd130be840f2",
+			chains: [ethChainId],
+			optionalChains: Object.keys(ethereumRpcMap)
+				.map((x) => +x)
+				.filter((x) => x !== ethChainId),
+			showQrModal: true,
+			methods: ["eth_sendTransaction", "personal_sign", "eth_signTypedData"],
+			optionalMethods: [
+				"eth_accounts",
+				"eth_requestAccounts",
+				"eth_sendRawTransaction",
+				"eth_sign",
+				"eth_signTransaction",
+				"eth_signTypedData",
+				"eth_signTypedData_v3",
+				"eth_signTypedData_v4",
+				"wallet_switchEthereumChain",
+				"wallet_addEthereumChain",
+				"wallet_getPermissions",
+				"wallet_requestPermissions",
+				"wallet_registerOnboarding",
+				"wallet_watchAsset",
+				"wallet_scanQRCode",
+			],
+			events: ["chainChanged", "accountsChanged"],
+			optionalEvents: ["message", "disconnect", "connect"],
+			rpcMap: ethereumRpcMap,
+		})
+	)
 
 	const phantomConnect = mapSolanaWallet(new PhantomConnectionProvider())
-	const solflareConnect = mapSolanaWallet(new SolflareConnectionProvider({
-		network: environment === "prod" ? "mainnet-beta" : "devnet",
-	}))
+	const solflareConnect = mapSolanaWallet(
+		new SolflareConnectionProvider({
+			network: environment === "prod" ? "mainnet-beta" : "devnet",
+		})
+	)
 
-	const imxConnector = mapImmutableXWallet(new ImmutableXLinkConnectionProvider({
-		env: environmentToImmutableXEnv(environment),
-	}))
-
+	const imxConnector = mapImmutableXWallet(
+		new ImmutableXLinkConnectionProvider({
+			env: environmentToImmutableXEnv(environment),
+		})
+	)
 
 	// Providers required secrets
 	// const fortmatic = mapEthereumWallet(new FortmaticConnectionProvider({ apiKey: "ENTER", ethNetwork: { chainId: 4, rpcUrl: "https://node-rinkeby.rarible.com" } }))
 	// const portis = mapEthereumWallet(new PortisConnectionProvider({ appId: "ENTER", network: "rinkeby" }))
 
-	const connector = Connector
-		.create(injected, state)
+	let connector = Connector.create(injected, state)
 		.add(nfid)
 		.add(walletLink)
 		.add(mew)
@@ -246,7 +294,11 @@ export function getConnector(environment: RaribleSdkEnvironment) {
 	// .add(fortmatic)
 
 	if (torus) {
-		return connector.add(torus)
+		connector = connector.add(torus)
+	}
+
+	if (firebase) {
+		connector = connector.add(firebase)
 	}
 
 	return connector
