8235e27bd95b231619580861f2b51182efc22d95
André Ginja <andginja@gmail.com>
2023-11-16 14:21:47 +0000

    fix(WL-2215): ignore engines for firebase connector
    Firebase build ignoring engines

--- scripts/build-connectors.sh
+++ scripts/build-connectors.sh
@@ -11,7 +11,7 @@ yarn run build-connector-mew &
 yarn run build-connector-portis &
 yarn run build-connector-fortmatic &
 yarn run build-connector-torus &
-yarn run build-connector-firebase &
+yarn --ignore-engines run build-connector-firebase &
 yarn run build-connector-walletlink &
 yarn run build-connector-walletconnect-v2 &
 yarn run build-connector-phantom &
