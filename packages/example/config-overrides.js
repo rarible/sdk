1f3fd46f41b132b4bb52a34648c3c8ed333b65f8
Eugene Nacu <evgeny.nacu@gmail.com>
2023-12-21 22:49:54 +0000

    fix(PLTF-203): fixed eslint, fixed build after typescript upgrade

--- packages/example/config-overrides.js
+++ packages/example/config-overrides.js
@@ -1,4 +1,4 @@
-const {override, addExternalBabelPlugins} = require("customize-cra")
+const { override, addExternalBabelPlugins, addWebpackAlias } = require("customize-cra")
 
 const configFn = override(
 	...addExternalBabelPlugins(
@@ -6,7 +6,17 @@ const configFn = override(
 		"@babel/plugin-proposal-logical-assignment-operators",
 		"@babel/plugin-proposal-optional-chaining",
 		"@babel/plugin-syntax-bigint"
-	)
+	),
+	addWebpackAlias({
+		"zlib": require.resolve("zlib-browserify"),
+		"os": require.resolve("os-browserify/browser"),
+		"crypto": require.resolve("crypto-browserify"),
+		"process/browser": require.resolve("process/browser"),
+		"stream": require.resolve("stream-browserify"),
+		"path": require.resolve("path-browserify"),
+		"https": false,
+		"http": false,
+	})
 )
 module.exports = function o(config) {
 	config.module.rules.push({
