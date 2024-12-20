2eb5255f1ac35167188a1b223c632904455d9032
Aleksandr Sozinov <a.sozinov@rarible.com>
2024-12-20 20:39:06 +0700

    Revert "feat: aptos balance from blockchain (#699)"
    This reverts commit d0db324dfd02475a1d7eedef02ed95bce4adc885.

--- packages/sdk/src/sdk-blockchains/aptos/balance.ts
+++ packages/sdk/src/sdk-blockchains/aptos/balance.ts
@@ -2,8 +2,7 @@ import type { AptosSdk } from "@rarible/aptos-sdk"
 import type { UnionAddress } from "@rarible/types"
 import type { BigNumber } from "@rarible/utils"
 import { toBn } from "@rarible/utils"
-import { extractId } from "@rarible/sdk-common"
-import { getCurrencyAssetType } from "../../common/get-currency-asset-type"
+import { getCurrencyId } from "../../common/get-currency-asset-type"
 import type { RequestCurrency } from "../../common/domain"
 import type { IApisSdk } from "../../domain"
 
@@ -16,13 +15,10 @@ export class AptosBalance {
   }
 
   async getBalance(address: UnionAddress, currency: RequestCurrency): Promise<BigNumber> {
-    const assetType = getCurrencyAssetType(currency)
-    const aptosAddress = extractId(address)
-
-    if (assetType["@type"] === "CURRENCY_NATIVE") {
-      const balance = await this.sdk.balance.getAptosBalance({ address: aptosAddress })
-      return toBn(balance)
-    }
-    throw new Error("Unsupported asset type")
+    const response = await this.apis.balances.getBalance({
+      currencyId: getCurrencyId(currency),
+      owner: address,
+    })
+    return toBn(response.decimal)
   }
 }
