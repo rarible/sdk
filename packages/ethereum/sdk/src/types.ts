f0b81aa102fcf0ab9faa5b9f82875ea5e2b759fc
Aleksandr Sozinov <sanzeting@mail.ru>
2025-01-17 17:35:39 +0700

    fix: ethereum networks (#708)
    Co-authored-by: Aleksandr Sozinov <a.sozinov@rarible.com>

--- packages/ethereum/sdk/src/types.ts
+++ packages/ethereum/sdk/src/types.ts
@@ -1,61 +1,11 @@
 import type { Binary, ConfigurationParameters } from "@rarible/ethereum-api-client"
 import type { Word } from "@rarible/types"
+import type { configDictionary } from "./config"
 
 export const ethereumSdkEnvironments = ["testnet", "production", "dev"] as const
 export type EthereumSdkEnvironment = (typeof ethereumSdkEnvironments)[number]
 
-export const ethereumNetworks = [
-  "mainnet",
-  "testnet",
-  "mumbai",
-  "polygon",
-  "dev-ethereum",
-  "dev-polygon",
-  "amoy-polygon",
-  "mantle",
-  "testnet-mantle",
-  "arbitrum",
-  "testnet-arbitrum",
-  "zksync",
-  "testnet-zksync",
-  "chiliz",
-  "testnet-chiliz",
-  "lightlink",
-  "testnet-lightlink",
-  "testnet-rari",
-  "rari",
-  "zkatana",
-  "astar-zkevm",
-  "astar-kyoto",
-  "base",
-  "base-sepolia",
-  "testnet-celo",
-  "celo",
-  "testnet-fief",
-  "testnet-xai",
-  "xai",
-  "testnet-kroma",
-  "kroma",
-  "sei-arctic-1",
-  "sei-pacific-1",
-  "moonbeam-testnet",
-  "moonbeam",
-  "palm-testnet",
-  "palm",
-  "etherlink-testnet",
-  "etherlink",
-  "lisk-sepolia",
-  "lisk",
-  "testnet-saakuru",
-  "saakuru",
-  "testnet-oasys",
-  "alephzero-testnet",
-  "alephzero",
-  "shape-testnet",
-  "shape",
-]
-
-export type EthereumNetwork = (typeof ethereumNetworks)[number]
+export type EthereumNetwork = keyof typeof configDictionary
 
 export enum LogsLevel {
   DISABLED = 0,
