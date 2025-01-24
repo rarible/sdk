bf060101ad21e79edc036e2d86e87b726915dbbd
i.yugfeld <i.yugfeld@rarible.com>
2025-01-24 13:01:39 +0200

    feat: add coverage report for tests

--- packages/sdk/scripts/test.sh
+++ packages/sdk/scripts/test.sh
@@ -4,13 +4,13 @@ start=$(date +%s)
 scripts_dir="$(cd "$(dirname "$0")" && pwd)"
 echo "scripts dir: $scripts_dir"
 cd "${scripts_dir}/.."
-jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/ethereum &
-jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/flow &
-jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/immutablex &
-jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/tezos
+jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/ethereum --coverage --coverageReporters=lcov &
+jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/flow --coverage --coverageReporters=lcov &
+jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/immutablex --coverage --coverageReporters=lcov &
+jest --runInBand --forceExit --testPathPattern=src/sdk-blockchains/tezos --coverage --coverageReporters=lcov
 wait
 echo "REST OF TESTS:"
-jest --runInBand --forceExit --testPathIgnorePatterns "src/sdk-blockchains/ethereum" "src/sdk-blockchains/immutablex" "src/sdk-blockchains/tezos" "src/sdk-blockchains/flow"
+jest --runInBand --forceExit --testPathIgnorePatterns "src/sdk-blockchains/ethereum" "src/sdk-blockchains/immutablex" "src/sdk-blockchains/tezos" "src/sdk-blockchains/flow" --coverage --coverageReporters=lcov
 
 
 end=$(date +%s)
