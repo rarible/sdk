c01b03b37b9c77d2847990feaae7b1adda6a224f
i.yugfeld <i.yugfeld@rarible.com>
2025-03-18 16:53:45 +0200

    feat: improve release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
 # Check for uncommitted changes and abort if found
 if [[ $(git status --porcelain) ]]; then
-    echo "There are uncommitted changes in your working directory. Please commit or stash them before running this script."
+    echo "${RED}There are uncommitted changes in your working directory. Please commit or stash them before running this script."
     exit 1
 fi
 
-echo "Checking out to master branch..."
+echo "${BLUE}Checking out to master branch..."
 git checkout master
 
-echo "Pulling latest changes..."
+echo "${BLUE}Pulling latest changes..."
 git pull origin master
 
-echo "Installing dependencies..."
+echo "${BLUE}Installing dependencies..."
 yarn
 
-echo "Build project..."
+echo "${BLUE}Build project..."
 yarn build
 
-echo "Running version command..."
+echo "${BLUE}Running version command..."
 yarn run version patch --yes --force-publish
 
-echo "Lerna version patch increment completed."
+echo "${BLUE}Lerna version patch increment completed."
+
+# Get the latest Git tag (created by Lerna)
+latest_tag=$(git describe --tags --abbrev=0)
+
+echo "${GREEN}Your build link: http://jenkins.rarible.int/view/protocol/job/protocol-sdk/view/tags/job/$latest_tag"
