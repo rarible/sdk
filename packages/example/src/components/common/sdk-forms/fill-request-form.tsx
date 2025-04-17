38a1fc4778705f30659d51d9e7c2ac62897b7efe
i.yugfeld <i.yugfeld@rarible.com>
2025-04-17 17:12:00 +0300

    feat: update api

--- packages/example/src/components/common/sdk-forms/fill-request-form.tsx
+++ packages/example/src/components/common/sdk-forms/fill-request-form.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react"
 import type { Order } from "@rarible/api-client"
-import { Box, IconButton, Stack } from "@mui/material"
+import { Stack } from "@mui/material"
 import type { PreparedFillInfo } from "@rarible/sdk/build/types/order/fill/domain"
 import type { UseFormReturn } from "react-hook-form"
-import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline"
-import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline"
 import { FormTextInput } from "../form/form-text-input"
 
 interface IFillRequestFormProps {
@@ -36,53 +34,10 @@ export function FillRequestForm(props: IFillRequestFormProps) {
         name={getFieldNameWithPrefix(namePrefix, "amount")}
         label="Amount"
       />
-      {isFillBatch && isAmmOrder(props.order) ? (
-        <>
-          {new Array(itemsCount).fill(0).map((v, i) => renderItemSelector({ ...props, key: i }))}
-          <Box display="flex" justifyContent="flex-end" alignItems="flex-end">
-            <IconButton
-              color="primary"
-              onClick={() => {
-                form.setValue(`${namePrefix}_itemsCounter`, itemsCount + 1)
-                setItemsCount(itemsCount + 1)
-              }}
-            >
-              <AddCircleOutlineIcon />
-            </IconButton>
-            <IconButton
-              color="error"
-              disabled={itemsCount <= 1}
-              onClick={() => {
-                form.setValue(`${namePrefix}_itemsCounter`, itemsCount - 1)
-                setItemsCount(Math.max(1, itemsCount - 1))
-              }}
-            >
-              <RemoveCircleOutlineIcon />
-            </IconButton>
-          </Box>
-        </>
-      ) : (
-        renderItemSelector({ ...props })
-      )}
     </Stack>
   )
 }
 
-function renderItemSelector({ form, order, namePrefix, key, isFillBatch }: IFillRequestFormProps) {
-  let fieldName = getFieldNameWithPrefix(namePrefix, "itemId")
-  if (isFillBatch) {
-    fieldName = fieldName + "_" + key
-  }
-  if (isAmmOrder(order)) {
-    return <FormTextInput type="text" key={key} form={form} name={fieldName} label="Item Id" />
-  }
-  return undefined
-}
-
 function getFieldNameWithPrefix(prefix: string | undefined, field: string): string {
   return (prefix ? prefix + "_" : "") + field
-}
-
-function isAmmOrder(order: Order | undefined): boolean {
-  return order?.make.type["@type"] === "AMM_NFT"
 }
