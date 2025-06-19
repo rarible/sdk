9875d7b560169a6bf31bb6f98bc0f0c4da4ce4a1
i.yugfeld <i.yugfeld@rarible.com>
2025-06-19 16:00:19 +0300

    feat: move signer

--- packages/ethereum/sdk/src/stability-protocol/stability-protocol.ts
+++ packages/ethereum/sdk/src/stability-protocol/stability-protocol.ts
@@ -7,18 +7,19 @@ import type { RaribleStabilityProtocolSdk, RaribleStabilityMessage } from "../in
 
 export class StabilityProtocol implements RaribleStabilityProtocolSdk {
   private provider: ethers.providers.JsonRpcProvider
+  private signer: ethers.Wallet
   private logger: RemoteLogger
   private destinationAddress: string
 
   constructor(apiKey: string, destinationAddress: string | undefined, logger: RemoteLogger) {
     const providerUrl = `https://rpc.stabilityprotocol.com/zgt/${apiKey}`
     this.provider = new ethers.providers.JsonRpcProvider(providerUrl)
+    this.signer = ethers.Wallet.createRandom().connect(this.provider)
     this.logger = logger
     this.destinationAddress = destinationAddress ?? EVM_ZERO_ADDRESS
   }
 
   sendMessage(message: RaribleStabilityMessage) {
-    const signer = ethers.Wallet.createRandom().connect(this.provider)
     const messageBytes = hexlify(toUtf8Bytes(JSON.stringify(message)))
     const transaction = {
       to: this.destinationAddress,
@@ -27,7 +28,7 @@ export class StabilityProtocol implements RaribleStabilityProtocolSdk {
       maxFeePerGas: 0,
     }
 
-    return signer
+    return this.signer
       .sendTransaction(transaction)
       .then(x => undefined)
       .catch(error => {
